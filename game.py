47a9efe256f2ab40c0110f1491b1bc82b385b37a
Kevin Moreno <moren162@mail.chapman.edu>
2019-08-06 10:19:52 -0700

    Created Deck class.

--- game.py
+++ game.py
@@ -5,33 +5,40 @@ king with ranks 1-13. '''
 
 
 
-
+import random
+from random import shuffle
 
 class Card:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
         
-    def get_rank:
+    def get_rank():
         return [ "Ace", "2", "3", "4", "5", "6", "7","8", "9", "10", "Jack", "Queen", "King" ]
 
-    def get_suit:
+    def get_suit():
         return ["Clubs", "Diamonds", "Hearts", "Spades"]
 
-    def get_value
+    def get_value():
         pass
  
-    def shuffle:
+    def shuffle():
         pass
 
-    def deal:
+    def deal():
         pass
 
-    def empty:
+    def empty():
         pass
 
     def __str__(self):
-        return self.rank
+        return self.rank + " of " + self.suit
+
+class Deck:
+    def __init__(self):
+        self.contents = []
+        self.contents = [ Card(rank,suit) for rank in get_rank() for suit in get_suits()]
+        random.shuffle(self.contents)
 
 
     
