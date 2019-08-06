a3454329b4bbe2034a2248267fd247df09fe1702
Kevin Moreno <moren162@mail.chapman.edu>
2019-08-06 10:09:43 -0700

    Class Card/get_rank/get_suit created.

---
+++ game.py
@@ -0,0 +1,38 @@
+#Solitaire
+'''Writing a program to play the popular card game of solitaire. Suites are represented
+by their first letter: 'S', 'D', 'H', 'C'. Each suit has 13 cards from ace,2-10,jack, queen,
+king with ranks 1-13. '''
+
+
+
+
+
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+        
+    def get_rank:
+        return [ "Ace", "2", "3", "4", "5", "6", "7","8", "9", "10", "Jack", "Queen", "King" ]
+
+    def get_suit:
+        return ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+    def get_value
+        pass
+
+    def shuffle:
+        pass
+
+    def deal:
+        pass
+
+    def empty:
+        pass
+
+    def __str__(self):
+        return self.rank
+
+
+    
+
